0f828b71aebe4acef88ae710124291ce62feaa4d
kmangutov <kmangutov@gmail.com>
2015-11-24 21:25:42 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+//this person is ____ who is _____
+var mapSocialCues = {
+  'a': ['an expert', 'in the target audience'],
+  'b': ['a non-expert', 'in the target audience'],
+  'c': ['an expert', 'not in the target audience'],
+  'd': ['a non-expert', 'not in the target audience']
+};
+
+$(document).ready(function(){
+  //canvasHandle = document.getElementById("canvas");
+  console.log('hello');
+  $('#textSocialCue').text('test');
+});
